33324cdac2607f1f1e2c8784cbb5d26b71e65823
Ian Cossentino <icossentino@gmail.com>
2020-11-11 16:36:07 -0500

    commit tree 6f98f4fade1f40039e34a6c9089c850e0f734d2a

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+
+
+
+class Dog
+    def initialize
+        puts "woof"
+    end
+end
+
+
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+
+
+
+class Person
+    def initialize
+        puts "woof"
+    end
+end
+
+
+
+adele_goldberg = Person.new
+alan_kay = Person.new
